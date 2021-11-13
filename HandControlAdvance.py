ad0088680a878b2a86253f744aaea9792a5870a9
Bandita <72542844+2bandita99@users.noreply.github.com>
2021-11-13 14:35:32 +0530

    Update HandControlAdvance.py

--- HandControlAdvance.py
+++ HandControlAdvance.py
@@ -3,9 +3,6 @@ import time
 import numpy as np
 import HandTrackingModule as htm
 import math
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 ################################
 wCam, hCam = 640, 480
@@ -18,21 +15,13 @@ pTime = 0
 
 detector = htm.handDetector(detectionCon=0.7, maxHands=1)
 
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(
-    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = cast(interface, POINTER(IAudioEndpointVolume))
-# volume.GetMute()
-# volume.GetMasterVolumeLevel()
-volRange = volume.GetVolumeRange()
-minVol = volRange[0]
-maxVol = volRange[1]
 vol = 0
 volBar = 400
 volPer = 0
 area = 0
 colorVol = (255, 0, 0)
-
+length =0
+cVal =0;
 while True:
     success, img = cap.read()
 
@@ -55,7 +44,7 @@ while True:
             volPer = np.interp(length,[50, 200], [0, 100])
 
             # Reduce Resolution to make it smoother
-            smoothness = 10
+            smoothness = 5
             volPer = smoothness * round(volPer / smoothness)
 
             # Check fingers up
@@ -64,7 +53,7 @@ while True:
 
             # If pinky is down set volume
             if not fingers[4]:
-                volume.SetMasterVolumeLevelScalar(volPer / 100, None)
+                cVal = volPer
                 cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
                 colorVol = (0, 255, 0)
             else:
@@ -73,10 +62,11 @@ while True:
     # Drawings
     cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
     cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
-    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
+    cv2.putText(img, f'{int(volPer)} ', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
                 1, (255, 0, 0), 3)
-    cVol = int(volume.GetMasterVolumeLevelScalar() * 100)
-    cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50), cv2.FONT_HERSHEY_COMPLEX,
+
+
+    cv2.putText(img, f'Voltage Set: {int(cVal)} ', (400, 50), cv2.FONT_HERSHEY_COMPLEX,
                 1, colorVol, 3)
 
     # Frame rate

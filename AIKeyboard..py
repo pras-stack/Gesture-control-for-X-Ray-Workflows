dcd52657e956344188e690683cbbcaaf770f59d2
Bandita <72542844+2bandita99@users.noreply.github.com>
2021-11-15 01:13:35 +0530

    Update AIKeyboard..py

--- AIKeyboard..py
+++ AIKeyboard..py
@@ -10,12 +10,13 @@ cap.set(3, 1280)
 cap.set(4, 720)
 
 detector = HandDetector(detectionCon=0.8)
-keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-        ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
-        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
-finalText = ""
+keys = [["1-20", "21-40", "41-60", "61-80"],
+        ["CHEST", "FOOT", "HEEL", "TOES"],
+        ["HIP", "KNEE", "ABD", "FEMUR","Enter"]]
+finalText = []
 
-keyboard = Controller()
+
+#keyboard = Controller()
 
 
 def drawAll(img, buttonList):
@@ -24,34 +25,13 @@ def drawAll(img, buttonList):
         w, h = button.size
         cvzone.cornerRect(img, (button.pos[0], button.pos[1], button.size[0], button.size[1]),
                           20, rt=0)
-        cv2.rectangle(img, button.pos, (x + w, y + h), (255, 0, 255), cv2.FILLED)
-        cv2.putText(img, button.text, (x + 20, y + 65),
-                    cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+        cv2.rectangle(img, button.pos, (x + w, y + h), (255, 153,51), cv2.FILLED)
+        cv2.putText(img, button.text, (x + 10, y + 40),
+                    cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2)
     return img
 
-
-#
-# def drawAll(img, buttonList):
-#     imgNew = np.zeros_like(img, np.uint8)
-#     for button in buttonList:
-#         x, y = button.pos
-#         cvzone.cornerRect(imgNew, (button.pos[0], button.pos[1], button.size[0], button.size[1]),
-#                           20, rt=0)
-#         cv2.rectangle(imgNew, button.pos, (x + button.size[0], y + button.size[1]),
-#                       (255, 0, 255), cv2.FILLED)
-#         cv2.putText(imgNew, button.text, (x + 40, y + 60),
-#                     cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
-#
-#     out = img.copy()
-#     alpha = 0.5
-#     mask = imgNew.astype(bool)
-#     print(mask.shape)
-#     out[mask] = cv2.addWeighted(img, alpha, imgNew, 1 - alpha, 0)[mask]
-#     return out
-
-
 class Button():
-    def __init__(self, pos, text, size=[85, 85]):
+    def __init__(self, pos, text, size=[100, 60]):
         self.pos = pos
         self.size = size
         self.text = text
@@ -74,28 +54,32 @@ while True:
             w, h = button.size
 
             if x < lmList[8][0] < x + w and y < lmList[8][1] < y + h:
-                cv2.rectangle(img, (x - 5, y - 5), (x + w + 5, y + h + 5), (175, 0, 175), cv2.FILLED)
-                cv2.putText(img, button.text, (x + 20, y + 65),
+                cv2.rectangle(img, (x - 5, y - 5), (x + w + 10, y + h + 10), (255, 153,51), cv2.FILLED)
+                cv2.putText(img, button.text, (x + 10, y + 15),
                             cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
                 l, _, _ = detector.findDistance(8, 12, img, draw=False)
                 print(l)
 
                 ## when clicked
                 if l <30:
-                    keyboard.press(button.text)
+                    #keyboard.press(button.text)
                     cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
                     cv2.putText(img, button.text, (x + 20, y + 65),
                                 cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
-                    finalText += button.text
-                    sleep(0.15)
+                    if(button.text != "Enter"):
+                        finalText += [button.text]
+                        sleep(0.15)
+                    if(button.text == "Enter"):
+                        cv2.putText(img, f'Ready to go!', (100, 390),
+                                    cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 3)
 
-    cv2.rectangle(img, (50, 350), (700, 450), (175, 0, 175), cv2.FILLED)
-    cv2.putText(img, finalText, (60, 430),
-                cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
+
+    cv2.rectangle(img, (50, 420), (440, 700), (255, 153,51), cv2.FILLED)
+    cv2.putText(img, str(finalText), (60, 460),
+                cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
-
 
 
 
